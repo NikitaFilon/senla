d7a74bde08474eaf5c3f5eefb3a9670a71212fe4
NikitaFilon <70878643+NikitaFilon@users.noreply.github.com>
2022-02-26 00:45:56 +0300

    Task one
    The first task is completed

---
+++ src/taskOne/CutomMath.java
@@ -0,0 +1,15 @@
+package taskOne;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CutomMath {
+    public static int sumOfNumber(String string) {
+        int result = 0;
+        Matcher matcher = Pattern.compile("\\d+").matcher(string);
+        while (matcher.find()) {
+           result += Integer.parseInt(matcher.group(0));
+        }
+        return result;
+    }
+}
